dd72e5e32879aad90c626a6136725d642c324a8b
SaraRamadan25 <sarahramadanelsayed@gmail.com>
2023-11-28 21:28:18 +0200

    Batches

--- app/Jobs/PullRepo.php
+++ app/Jobs/PullRepo.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use Illuminate\Bus\Batchable;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -9,9 +10,9 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class pullRepo implements ShouldQueue
+class PullRepo implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Batchable,Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     /**
      * Create a new job instance.
@@ -26,6 +27,9 @@ class pullRepo implements ShouldQueue
      */
     public function handle(): void
     {
-        //
+        // when a job inside a batch is cancelled, the batch all is cancelled " It is the default approach, we can override this"
+        if ($this->batch()->cancelled()) {
+            return;
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Jobs\Deploy;
 use App\Jobs\ProcessPayment;
-use App\Jobs\pullRepo;
+use App\Jobs\PullRepo;
 use App\Jobs\runTests;
 use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Route;
@@ -21,13 +21,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     //first type : chaining
     //second type : batches
-    $chain = [
-        new pullRepo(),
-        new runTests(),
-        new deploy(),
+    $batch = [
+        new PullRepo('laracasts/project1'),
+        new PullRepo('laracasts/project2'),
+        new PullRepo('laracasts/project3'),
         ];
 
-    Bus::chain($chain)->dispatch();
+    Bus::batch($batch)
+        ->allowFailures()
+        ->dispatch();
 
     return view('welcome');
 });
