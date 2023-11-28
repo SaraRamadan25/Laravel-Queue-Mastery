f1110581542acbc7468141f1488354444118be2d
SaraRamadan25 <sarahramadanelsayed@gmail.com>
2023-11-28 18:41:31 +0200

    chaining

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Jobs\Deploy;
 use App\Jobs\ProcessPayment;
-use App\Jobs\SendWelcomeEmail;
+use App\Jobs\pullRepo;
+use App\Jobs\runTests;
+use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -10,16 +13,21 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
+| routes are loaded by the RouteServiceProvider, and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
-    foreach (range(1, 10) as $i) {
-        SendWelcomeEmail::dispatch();
-    }
-    ProcessPayment::dispatch()->onQueue('payments');
+    //first type : chaining
+    //second type : batches
+    $chain = [
+        new pullRepo(),
+        new runTests(),
+        new deploy(),
+        ];
+
+    Bus::chain($chain)->dispatch();
 
     return view('welcome');
 });
